04674242d3dbe05d1ac8f81bd1ad06171331b6d2
Jahongir <sjahongir22@gmail.com>
2024-02-16 20:43:45 +0500

    Problem_6_Zigzag_Conversion

--- src/main/java/org/example/medium/Problem_6_Zigzag_Conversion.java
+++ src/main/java/org/example/medium/Problem_6_Zigzag_Conversion.java
@@ -5,5 +5,12 @@ public class Problem_6_Zigzag_Conversion {
       Path :
      https://leetcode.com/problems/zigzag-conversion/
       */
+    public static void main(String[] args) {
+
+    }
+
+    public static String convert(String s, int numRows) {
+
+    }
 
 }
