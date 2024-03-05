9d19ce4907d6791d64349389933cd9389310e3c3
JahongirDevelop <sjahongir221@gmail.com>
2024-03-05 20:19:41 +0500

    Problem_20_ValidParentheses

--- src/main/java/org/example/easy/Problem_20_ValidParentheses.java
+++ src/main/java/org/example/easy/Problem_20_ValidParentheses.java
@@ -3,7 +3,16 @@ package org.example.easy;
 import java.util.Stack;
 
 public class Problem_20_ValidParentheses {
-    public boolean isValid(String s) {
+
+    /*
+      Path :
+      https://leetcode.com/problems/valid-parentheses
+      */
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+    public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '[' || c == '{') {
